2807a76b167e7fc838b406ada92a227f44db1668
Phoen1x15 <162161203+whoisnarik@users.noreply.github.com>
2024-03-25 13:27:55 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print ("Hello world")
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from decouple import config
+from handlers import private, group, homeworks
+from optional import options
+
+
+async def main():
+    bot = Bot(token=config('TOKEN'), parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+    dp.include_routers(private.private_router, group.group_router, homeworks.homework_router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    # await bot.delete_my_commands()
+    await bot.set_my_commands(commands=options.private)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
